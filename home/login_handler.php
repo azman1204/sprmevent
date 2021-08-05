b0a3a5e6bf33ac4cc9ebae5415afa5bcc0b9d36f
Azman Zakaria <azman1204@yahoo.com>
2021-08-05 15:44:43 +0800

    login selesai

--- home/login_handler.php
+++ home/login_handler.php
@@ -1,4 +1,27 @@
 <?php
+session_start(); // mesti panggil func. ini sebelum guna session
+include '../include/conn.php';
 $user_id = $_POST['user_id'];
 $password = $_POST['password'];
 echo "$user_id $password";
+$password_hash = password_hash($password, PASSWORD_BCRYPT);
+$sql = "SELECT * FROM user WHERE user_id = '$user_id'";
+$rs = mysqli_query($conn, $sql);
+if ($row = mysqli_fetch_array($rs)) {
+    // user id match
+    if (password_verify($password, $row['password'])) {
+        // password match
+        echo "user exist";
+        $_SESSION['user_id'] =$user_id; // set session $_GET $_POST $_SESSION -> assoc. array
+        header('location:../event/index.php');
+    } else {
+        echo "password does not match";
+        exit;
+    }
+} else {
+    // user id x match
+    echo "User does not exist";
+    exit;
+}
+
+
